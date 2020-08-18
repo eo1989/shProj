8bd2b3cb8f574e1a73f08b09b01d8c1ff54f00a2
eo1989 <eorlowski6@gmail.com>
2020-08-18 13:20:44 -0400

    added 'backdoor' script to FZF-RG.sh

---
+++ FZF-RG.sh
@@ -0,0 +1,27 @@
+#!/usr/local/zsh
+
+if [[$1 == -f ]]; then
+    shift
+    local fifo
+    exec {fifo}<>$1
+    backdoor -i $fifo
+elif [[ $1 == -i  ]]; then
+    shift
+    zle -F $1 backdoor
+elif [[ $1 == <->  ]]; then
+    local line
+    # can get DoSed by someone writing data w/o a newline,
+    # but obv. that's the least of ur prob's if the other end isn't trusted.
+    if ! IFS= read -r line <&$1; then
+        zle -F $1
+        return 1
+    fi
+    eval $line
+else
+    echo >&2 "Usage: backdoor -f fifo"
+    echo >&2 "       backdoor -i fd"
+    echo >&2
+    echo >&2 "Will read lines from given fifo & eval them in zle -F context"
+    echo >&2 "You can also attach it to an fd urself w/ backdoor -i fd, &"
+    echo >&2 "it'll run all lines read from FD."
+fi
