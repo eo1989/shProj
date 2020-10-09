16d0ecf4465a3e972a95af1248bf19d81a8fa43b
eo1989 <eorlowski6@gmail.com>
2020-10-09 11:14:03 -0400

    need to add this to .zshrc so it's sourced & using the last command output can be injected into terminal

---
+++ last-command.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh zsh
+# Use Ctrl-x,Ctrl-l to get the output of the last command
+
+zmodload -i zsh/parameter
+insert-last-command-output() {
+LBUFFER+="$(eval $history[$((HISTCMD-1))])"
+}
+zle -N insert-last-command-output
+bindkey "^X^L" insert-last-command-output
